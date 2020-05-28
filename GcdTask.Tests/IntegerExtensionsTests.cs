7a548cd25330f034c8a21301f3a7d7d441080c1f
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-28 12:59:42 +0000

    Update IntegerExtensionsTests.cs

--- GcdTask.Tests/IntegerExtensionsTests.cs
+++ GcdTask.Tests/IntegerExtensionsTests.cs
@@ -14,7 +14,7 @@ namespace GcdTask.Tests
         [TestCase(2672, 5678, ExpectedResult = 334)]
         [TestCase(10927782, 6902514, ExpectedResult = 846)]
         [TestCase(1590771464, 1590771620, ExpectedResult = 4)]
-        public int FinGcd_With_All_Positive_Numbers(int a, int b) => FindGcd(a, b);
+        public int FinGcd_WithAllPositiveNumbers(int a, int b) => FindGcd(a, b);
 
         [TestCase(30, -12, ExpectedResult = 6)]
         [TestCase(10927782, -6902514, ExpectedResult = 846)]
@@ -22,19 +22,19 @@ namespace GcdTask.Tests
         [TestCase(1590771464, -1590771620, ExpectedResult = 4)]
         [TestCase(-10234567, -234568989, ExpectedResult = 97)]
         [TestCase(-10234562, -7872334, ExpectedResult = 2)]
-        public int FinGcd_With_Some_Negative_Numbers(int a, int b) => FindGcd(a, b);
+        public int FinGcd_WithSomeNegativeNumbers(int a, int b) => FindGcd(a, b);
 
         [TestCase(945, 0, ExpectedResult = 945)]
         [TestCase(0, -301, ExpectedResult = 301)]
         [TestCase(10927782, 0, ExpectedResult = 10927782)]
         [TestCase(-1590771464, 0, ExpectedResult = 1590771464)]
-        public int FinGcd_With_One_Zero_Number(int a, int b) => FindGcd(a, b);
+        public int FinGcd_WithOneZeroNumber(int a, int b) => FindGcd(a, b);
 
         [TestCase(int.MaxValue, int.MinValue, ExpectedResult = 1)]
-        public int FinGcd_With_Max_And_Min_Integer_Numbers(int a, int b) => FindGcd(a, b);
+        public int FinGcd_WithMaxAndMinIntegerNumbers(int a, int b) => FindGcd(a, b);
 
         [Test]
-        public void FinGcd_With_Two_Zero_Numbers_Throw_ArgumentException() =>
+        public void FinGcd_WithTwoZeroNumbers_ThrowArgumentException() =>
             Assert.Throws<ArgumentException>(() => FindGcd(0, 0),
                 "Two numbers cannot be 0 at the same time.");
     }
