e1da7342dc64a6a0511fd003143f931f3061bfa0
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-29 11:50:44 +0000

    Update IntegerExtensions.cs

--- GcdTask/IntegerExtensions.cs
+++ GcdTask/IntegerExtensions.cs
@@ -6,6 +6,14 @@ namespace GcdTask
     {
         public static int FindGcd(int a, int b)
         {
+            /// <summary>
+            /// Calculates GCD of two integers by the Euclidean algorithm.
+            /// </summary>
+            /// <param name="a">First integer.</param>
+            /// <param name="b">Second integer.</param>
+            /// <returns>The GCD value.</returns>
+            /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.
+            /// -or- Thrown when one of the arguments is int.MinValue.</exception>
             throw new NotImplementedException("You need to implement this function.");
         }
     }
