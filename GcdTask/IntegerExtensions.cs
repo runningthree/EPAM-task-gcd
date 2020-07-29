6887efcf9ef11f457281609fc977c1044307c67e
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-29 18:27:11 +0000

    Replace IntegerExtensions.cs

--- GcdTask/IntegerExtensions.cs
+++ GcdTask/IntegerExtensions.cs
@@ -4,17 +4,17 @@ namespace GcdTask
 {
     public static class IntegerExtensions
     {
+        /// <summary>
+        /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue]  by the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
         public static int FindGcd(int a, int b)
         {
-            /// <summary>
-            /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue]  by the Euclidean algorithm.
-            /// </summary>
-            /// <param name="a">First integer.</param>
-            /// <param name="b">Second integer.</param>
-            /// <returns>The GCD value.</returns>
-            /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
-            /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
-            throw new NotImplementedException("You need to implement this function.");
+            throw new NotImplementedException("You need to implement this method.");
         }
     }
 }
